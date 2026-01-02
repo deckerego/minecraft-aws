90e935cf0e1c6d6ff66fca8c9de110951a5d023d
John Ellis <532789+deckerego@users.noreply.github.com>
2026-01-02 18:53:36 -0500

    bash is more forgiving than I deserve

--- scripts/get_server.sh
+++ scripts/get_server.sh
@@ -2,12 +2,11 @@
 DESTINATION='/opt/minecraft'
 TEMP_DIR='.'
 
-MANIFEST=$(curl -s https://piston-meta.mojang.com/mc/game/version_manifest.json)
+MANIFEST=$(curl -s "https://piston-meta.mojang.com/mc/game/version_manifest.json")
 LATEST=$(echo "$MANIFEST" | jq -r '.["latest"]["release"]')
 echo "Getting metadata for $LATEST"
 
 LATEST_URL=$(echo "$MANIFEST" | jq -r '.["versions"] | map(select(.["id"] == "'"$LATEST"'"))[0] | .["url"]')
-echo $LATEST_URL
 METADATA=$(curl -s "$LATEST_URL")
 DOWNLOAD_HASH=$(echo $METADATA | jq -r '.["downloads"]["server"]["sha1"]')
 DOWNLOAD_URL=$(echo $METADATA | jq -r '.["downloads"]["server"]["url"]')
@@ -15,14 +14,14 @@ echo "Downloading $DOWNLOAD_URL"
 
 echo 'eula=true' > "$DESTINATION/eula.txt"
 curl -o "$TEMP_DIR/server.jar" "$DOWNLOAD_URL"
-echo "Verifying $HASH"
 CALCULATED_HASH=$(sha1sum "$TEMP_DIR/server.jar")
+echo "Verifying $CALCULATED_HASH"
 
 if [[ "$CALCULATED_HASH" = "$DOWNLOAD_HASH  $TEMP_DIR/server.jar" ]]; then
     echo "Updating $DESTINATION/server.jar"
-    cp "$TEMP_DIR/server.jar" "$DESTINATION/server.jar"
+    mv "$TEMP_DIR/server.jar" "$DESTINATION/server.jar"
     exit 0
-fi
+else
     echo "*** INVALID FILE, EXITING ***"
     exit -1
-done
+fi
