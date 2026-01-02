c008b4ea48075fba0a2db2a790eb3398c5363167
John Ellis <532789+deckerego@users.noreply.github.com>
2026-01-02 18:47:00 -0500

    Download the latest mincraft server JAR

--- scripts/get_server.sh
+++ scripts/get_server.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+#DESTINATION='/opt/minecraft'
+DESTINATION='.'
+TEMP_DIR='.'
 
-HASH=64bb6d763bed0a9f1d632ec347938594144943ed
+MANIFEST=$(curl -s https://piston-meta.mojang.com/mc/game/version_manifest.json)
+LATEST=$(echo "$MANIFEST" | jq -r '.["latest"]["release"]')
+echo "Getting metadata for $LATEST"
 
-echo 'eula=true' > /opt/minecraft/eula.txt
-curl -o /opt/minecraft/server.jar https://piston-data.mojang.com/v1/objects/${HASH}/server.jar
-echo "${HASH}  /opt/minecraft/server.jar" | sha1sum -c -
+LATEST_URL=$(echo "$MANIFEST" | jq -r '.["versions"] | map(select(.["id"] == "'"$LATEST"'"))[0] | .["url"]')
+echo $LATEST_URL
+METADATA=$(curl -s "$LATEST_URL")
+DOWNLOAD_HASH=$(echo $METADATA | jq -r '.["downloads"]["server"]["sha1"]')
+DOWNLOAD_URL=$(echo $METADATA | jq -r '.["downloads"]["server"]["url"]')
+echo "Downloading $DOWNLOAD_URL"
+
+echo 'eula=true' > "$DESTINATION/eula.txt"
+curl -o "$TEMP_DIR/server.jar" "$DOWNLOAD_URL"
+echo "Verifying $HASH"
+CALCULATED_HASH=$(sha1sum "$TEMP_DIR/server.jar")
+
+if [[ "$CALCULATED_HASH" = "$DOWNLOAD_HASH  $TEMP_DIR/server.jar" ]]; then
+    echo "Updating $DESTINATION/server.jar"
+    cp "$TEMP_DIR/server.jar" "$DESTINATION/server.jar"
+    exit 0
+fi
+    echo "*** INVALID FILE, EXITING ***"
+    exit -1
+done
